ed4eb62c80dc79ef86ded247e4e4308878cbd49a
RehanAfridikkk <rehan.gull12345@gmail.com>
2023-11-17 16:07:34 +0500

    non comment

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 here we are counting the no of Words/vowels/punctuations/lines/time-coplexcity through an API call (Echo Web-FrameWork)
 It just uses a Post request to get the "FilePath" and "No-of-Routines" and returns the total-count,total-routines and time taken
 to see the "pkg/count.go" file for the conditions of counts 
-for reference I have provided a commented code in the "word-count-API-echo.go" uncomment it and it shows how a simple get and post request work 
+
--- word-count-API-echo.go
+++ word-count-API-echo.go
@@ -1,70 +1,3 @@
-/*package main
-
-import (
-    "net/http"
-
-
-
-
-    "github.com/labstack/echo/v4"
-)
-
-type Message struct {
-    Text string `json:"text"`
-}
-
-func main() {
-    e := echo.New()
-
-    e.GET("/", func(c echo.Context) error {
-        return c.JSON(http.StatusOK, map[string]string{"message": "Hello, World!"})
-    })
-
-
-    e.POST("/post", func(c echo.Context) error {
-        // Parse JSON request body
-        var message Message
-        if err := c.Bind(&message); err != nil {
-            return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request payload"})
-        }
-
-        return c.JSON(http.StatusOK, map[string]string{"received": message.Text})
-    })
-
-    e.Logger.Fatal(e.Start(":1323"))
-}
-*/
-/*
-package main
-
-import (
-
-	"github.com/labstack/echo/v4"
-	"net/http"
-)
-type Message struct{
-    Text string `json :"text"`
-}
-
-func main()  {
-    e:= echo.New()
-
-    e.GET("/" ,func(c echo.Context) error{
-        return c.JSON(http.StatusOK , map[string]string{"message":"hello rehan"})
-    })
-
-
-    e.POST("/post" , func(c echo.Context) error {
-        var message Message
-        if err :=c.Bind(&message); err!= nil{
-            return c.JSON(http.StatusBadRequest ,map[string]string{"error":"invaled values or parameters"})
-        }
-        return c.JSON(http.StatusOK , map[string]string{"received":message.Text})
-
-    })
-    e.Logger.Fatal(e.Start(":1330"))
-}
-*/
 
 package main
 
@@ -82,13 +15,12 @@ type Message struct {
 
 
 
-
 func main() {
 	e := echo.New()
 
 	
 	e.POST("/post", postData)
-	// e.GET("/get", getData)
+	
 
 	
 	e.Logger.Fatal(e.Start(":8080"))
